555f1110892115e366b73a5d085251494c0a3178
John Bright <jmbright400@gmail.com>
2023-10-16 13:47:09 -0500

    exercise completed

---
+++ FactoryPattern/Motorbike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class Motorbike : IVehicle
+    {
+        public void Drive ()
+        {
+            Console.WriteLine("Built a motorbike to ride!");
+        }
+    }
+}
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter number of tires please!");
+            if (int.TryParse(Console.ReadLine(), out int numTires))
+            {
+                try
+                {
+                    IVehicle vehicle = VehicleFactory.GetVehicle(numTires);
+                    vehicle.Drive();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unexpected input. Please enter an expected number of tires.");
+            }
         }
     }
 }
